7d665c268d91ef6fe9edc6739891537629a2be71
shangyulu <93262763+shangyulu@users.noreply.github.com>
2021-12-08 20:49:58 +0800

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,11 @@
+# shang
+n=["Melody","york","tracy","angel"]
+v=["dancing","eating","jumpung","cooking"]
+p=['store',"bathroom","bedroom","refrigerator"]
+
+import random
+print(random.sample(n,1))
+print("is")
+print(random.sample(v,1))
+print("in")
+print(random.sample(p,1))
